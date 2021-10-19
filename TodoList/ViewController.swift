8d933f5f5b6f781ba78c7e4aa41fe2cc9ca2fb13
Minji Kim <minji_0801@naver.com>
2021-10-19 13:03:31 +0900

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by 김민지 on 2021/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
